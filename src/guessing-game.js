226358e49d91b1eaaac098448e1dbaae41d147d9
AR1D <84451649+Alex06052003@users.noreply.github.com>
2024-10-19 18:19:30 +0800

    Add files via upload

---
+++ src/guessing-game.js
@@ -0,0 +1,26 @@
+class GuessingGame {
+    constructor() {
+        this.min = 0;
+        this.max = 0;
+    }
+
+    setRange(min, max) {
+        this.min = min;
+        this.max = max;
+    }
+
+    guess() {
+        const middle = Math.round((this.min + this.max) / 2);
+        return middle;
+    }
+
+    lower() {
+        this.max = this.guess();
+    }
+
+    greater() {
+        this.min = this.guess();
+    }
+}
+
+module.exports = GuessingGame;
